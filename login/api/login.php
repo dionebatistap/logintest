8664f46501d450d0b97e7b492228ff6ec34b1c3f
dionebatistap <dionebatistap@gmail.com>
2021-06-14 11:56:21 -0300

    Versão 1.0.0
    # Implementação banco de dados tabela usuario
    # Implementação das Apis de login
    # Implementação da tela de login
    # Implementação da função de login

---
+++ login/api/login.php
@@ -0,0 +1,32 @@
+<?php
+
+require "../config/connect.php";
+
+if ($_SERVER['REQUEST_METHOD']=="POST"){
+    #CODE
+    $response = array();
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    
+    $cek = "SELECT * FROM users WHERE username='$username' and password='$password'";
+    $result = mysqli_fetch_array(mysqli_query($con, $cek));
+
+    if (isset($result)) {
+        # code...
+        $response['value']=1;
+        $response['message']="Login bem sucedido";
+        echo json_encode($response);
+
+    } else {
+        # code...
+        $response['value']=0;
+        $response['message']="Falha no login";
+        echo json_encode($response);
+    }
+    
+
+    
+
+}
+
+?>
---
+++ login/config/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+    define('HOST', 'mysql742.umbler.com');
+    define('USER', 'dione');
+    define('PASS','ak910627');
+    define('DB','apitestedione');
+
+    $con = mysqli_connect(HOST,USER,PASS,DB) or die('Falha ao conectar')
+
+?>

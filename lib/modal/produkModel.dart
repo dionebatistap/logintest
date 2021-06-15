689ed765607581762bf2a88c7363c7039a9fcde3
dionebatistap <dionebatistap@gmail.com>
2021-06-15 08:33:56 -0300

    API/Model Listar produto
    Criação da api e model para listar produto

---
+++ lib/modal/produkModel.dart
@@ -0,0 +1,22 @@
+class ProdukeModel {
+  
+  final String id;
+  final String namaProduk;
+  final String qty;
+  final String harga;
+  final String createDate;
+  final String idUsers;
+  final String nama;
+
+  ProdukeModel(
+  this.id,
+  this.namaProduk,
+  this.qty,
+  this.harga,
+  this.createDate,
+  this.idUsers,
+  this.nama
+  );
+
+
+}
---
+++ login/api/lihatProduk.php
@@ -0,0 +1,28 @@
+<?php
+
+require "../config/connect.php";
+
+
+
+    $response = array();
+
+    $sql = mysqli_query($con, "SELECT a.*, b.nama FROM produk a
+    left join users b on a.idUsers = b.id");
+    while ($a = mysqli_fetch_array($sql)) {
+        # code...
+        $b['id'] = $a['id'];
+        $b['namaProduk'] = $a['namaProduk'];
+        $b['qty'] = $a['qty'];
+        $b['harga'] = $a['harga'];
+        $b['createdDate'] = $a['createdDate'];
+        $b['idUsers'] = $a['idUsers'];
+        $b['nama'] = $a['nama'];
+
+        array_push($response, $b);
+
+    }
+
+    echo json_encode($response);
+
+
+?>

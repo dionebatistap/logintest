6c1eff0daa60df5ae98609a7af4101cf12967691
dionebatistap <dionebatistap@gmail.com>
2021-06-16 08:57:17 -0300

    GRIDVIEW
    IMPLEMENTAÇÃO GRIDVIEW

--- lib/views/menuUsers.dart
+++ lib/views/menuUsers.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:logintest/modal/api.dart';
+import 'package:logintest/modal/produkModel.dart';
+import 'package:http/http.dart' as http;
 
 class MenuUsers extends StatefulWidget {
   final VoidCallback signOut;
+
   MenuUsers(this.signOut);
 
   @override
@@ -9,11 +16,58 @@ class MenuUsers extends StatefulWidget {
 }
 
 class _MenuUsersState extends State<MenuUsers> {
+  final money = NumberFormat("#,##0", "en_US");
+
+  var loading = false;
+  final list = new List<ProdukModel>();
+  final GlobalKey<RefreshIndicatorState> _refresh =
+      GlobalKey<RefreshIndicatorState>();
+  Future<void> _lihatData() async {
+    list.clear();
+    if (!mounted) return;
+    setState(() {
+      loading = true;
+    });
+    final response = await http.get(BaseUrl.lihatProduk);
+    if (response.contentLength == 2) {
+    } else {
+      final data = jsonDecode(response.body);
+      data.forEach((api) {
+        final ab = new ProdukModel(
+          api['id'],
+          api['namaProduk'],
+          api['qty'],
+          api['harga'],
+          api['createdDate'],
+          api['idUsers'],
+          api['nama'],
+          api['image'],
+        );
+        list.add(ab);
+      });
+      if (!mounted) return;
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _lihatData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
           IconButton(
             onPressed: () {
               setState(() {
@@ -24,11 +78,42 @@ class _MenuUsersState extends State<MenuUsers> {
           ),
         ],
       ),
-      body: Container(
-        child: Center(
-          child: Text("Menu / Cliente"),
-        ),
-      ),
+      body: Container(child: OrientationBuilder(
+        builder: (context, orientation) {
+          return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+              ),
+              itemCount: list.length,
+              itemBuilder: (context, i) {
+                final x = list[i];
+                return Card(
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Image.network(
+                          BaseUrl.upload +
+                              x.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      Text(
+                        x.namaProduk,
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "RS" + money.format(int.parse(x.harga)),
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
+      )),
     );
   }
 }
